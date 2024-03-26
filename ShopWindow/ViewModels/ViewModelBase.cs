b7cd32c72ce3e3db03ce4b508c90fd9480f23259
prdb <prdb@DESKTOP-F6U7JA1>
2024-03-26 12:04:52 +0300

    Добавьте файлы проекта.

---
+++ ShopWindow/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace ShopWindow.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ ShopWindow/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ShopWindow.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
